2b03922f580799e9a049869c2bf0b98a4e9277e9
Melones5 <alexis_32@live.com.ar>
2022-07-08 11:01:24 -0300

    finalfuncional

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <BrowserRouter>
       <Navigation />
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact} />
+          <Route path="/" exact  element={<Home/>} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" c element={<Contact/>} />
           <Route component={Error}/>
         </Routes>
     </BrowserRouter>
