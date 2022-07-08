fca61ae11275d945aae3551a21f93b474067c38a
Melones5 <alexis_32@live.com.ar>
2022-07-07 21:28:36 -0300

    primero

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Home from './components/Home';
+import About from './components/About';
+import Error from './components/Error';
+import Contact from './components/Contact';
+import Navigation from './components/Navigation';
+
+import {Routes, Route, BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact} />
+          <Route component={Error}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
